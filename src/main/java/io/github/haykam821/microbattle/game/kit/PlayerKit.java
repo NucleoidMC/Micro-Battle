362ff20394c190e0b066d823ce418ba91c500ec5
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-12-10 09:10:51 -0500

    Make the armor of the player kit match the team color of the player

--- src/main/java/io/github/haykam821/microbattle/game/kit/PlayerKit.java
+++ src/main/java/io/github/haykam821/microbattle/game/kit/PlayerKit.java
@@ -4,17 +4,20 @@ import io.github.haykam821.microbattle.game.PlayerEntry;
 import net.minecraft.util.DyeColor;
 
 public class PlayerKit extends Kit {
+	private final int color;
+
 	public PlayerKit(PlayerEntry entry) {
 		super(KitTypes.PLAYER, entry);
+		this.color = this.entry.getTeam() == null ? DyeColor.RED.getFireworkColor() : this.entry.getTeam().getFireworkColor();
 	}
 	
 	@Override
 	protected int getBaseColor() {
-		return DyeColor.RED.getFireworkColor();
+		return this.color;
 	}
 
 	@Override
 	protected int getSecondaryColor() {
-		return DyeColor.RED.getFireworkColor();
+		return this.color;
 	}
 }
